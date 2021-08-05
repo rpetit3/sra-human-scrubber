48e5a3acc401d112e0fed7f1595147db423347cc
Robert A. Petit III <robbie.petit@gmail.com>
2021-08-04 18:33:41 -0600

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter.db" -o ./data/human_filter.db
+curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter.db" -o ${SCRUBBER_SHARE}/data/human_filter.db
 exit 0
