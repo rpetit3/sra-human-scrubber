493d7a6af5a68a6e9ed6ea2b4ca8c741330ba768
Robert A. Petit III <robbie.petit@gmail.com>
2022-09-19 12:28:15 -0600

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eu
 
+SCRUBBER_SHARE=BIOCONDA_SED_REPLACE
 VERSION=$(curl "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
-cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
-ln -s "${VERSION}".human_filter.db human_filter.db
+cd ${SCRUBBER_SHARE}/data && \
+  curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db" &&
+  ln -s -f "${VERSION}".human_filter.db human_filter.db
+
 exit 0
